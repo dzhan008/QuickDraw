3aa3194ba8f57753274e2a16b4bb557c2cf94273
Darren Houn <dhoun001@ucr.edu>
2018-03-13 17:36:16 -0700

    changed bracket events file

--- app/events/bracket_events.py
+++ app/events/bracket_events.py
@@ -3,8 +3,7 @@ from flask_socketio import send, emit
 from .. import flask_app, socketio
 from ..classes import helper
 
-#@socketio.on('gameReady')
-@flask_app.route('/matchmaking')
+@socketio.on('gameReady')
 def matchmaking(gameCode):
     reference = LobbyManager.getGameManager(gameCode)
     reference.choosePlayers()
